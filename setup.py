8f385fd9f7e05c97c4f4d82c1a18250fdbe27945
Muneeb Ahmed <muneebahmad25211@gmail.com>
2026-02-19 01:22:57 +0500

    Created a Logger

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Muneeb Ahmed',
     author_email='muneebahmad25211@gmail.com',
-    install_requires=['openai','langchain','stremlit','python-dotenv','PyPDF2'],
+    install_requires=['openai','langchain','streamlit','python-dotenv','PyPDF2'],
     packages=find_packages()
 )
